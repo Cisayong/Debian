e75b85725c7af21c8b89c2cb6e0ff9ef81bf10d0
Cisayong <55373421+Cisayong@users.noreply.github.com>
2019-09-16 21:17:18 +0700

    Add files via upload

---
+++ Debian.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# Variables
+b='\033[1m'
+u='\033[4m'
+bl='\E[30m'
+r='\E[31m'
+g='\E[32m'
+bu='\E[34m'
+m='\E[35m'
+c='\E[36m'
+w='\E[37m'
+endc='\E[0m'
+enda='\033[0m'
+blue='\e[1;34m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+
+python2 login.py
+clear
+echo -e $bl"."
+echo -e $bu  "~•~•~•~•~•~•~•~•~•~•~•~•~•~•~•~•~•~•~•~•~•~•~"
+echo -e $bu  " Tools       : RandiSr$white          " 
+echo -e $bu  " Author      : Mr.NEWBIE3X$white  " 
+echo -e $bu  " YouTube     : RANDI OLOYY$white " 
+echo -e $bu  "~•~•~•~•~•~•~•~•~•~•~•~•~•~•~•~•~•~•~•~•~•~•~" 
+sleep 5
+clear
+python2 install.py
+clear
+pkg install php
+pkg install wget
+pkg install figlet
+pkg install toilet
+pkg install python
+pkg install ruby
+PKG install wget openssl-tool proot -y
+gem install lolcat
+pip2 install requests
+pip2 install mechanize
+pip2 install tqdm
+pip2 install termcolor
+pkg install wget openssl-tool proot -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Debian/debian.sh && bash debian.sh
+./start-debian.sh
